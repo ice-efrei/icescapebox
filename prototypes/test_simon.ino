cdfe5ee63efa84c6fbcbeb7a087425c774146dc3
Datruc21 <jalil.bellahcen@gmail.com>
2024-12-03 16:01:37 +0100

    Update test_simon.ino

--- prototypes/test_simon.ino
+++ prototypes/test_simon.ino
@@ -6,7 +6,7 @@ int lives = 3;
 void setup() {
   Serial.begin(9600);
   // Génération de la séquence aléatoire
-  for (int j = 0; j < 10; j++) {
+ for (int j = 0; j < 10; j++) {
     int randomNumber = rand() % 4; // Génère un nombre entre 0 et 3
     char c = '0' + randomNumber;  // Convertit en caractère ('0', '1', '2', '3')
     answer[j] = c;                // Ajoute directement au tableau
@@ -21,19 +21,20 @@ void loop() {
     Serial.print("Votre séquence actuelle : ");
     Serial.println(answer); // Pour afficher la séquence complète
 
-    // Lecture de l'entrée utilisateur
-    if (Serial.available() > 0) {
-      int numero = Serial.parseInt(); // Récupère un nombre entier depuis le port série
+    Serial.println("Entrez un nombre :"); // Ajout d'une invite pour l'utilisateur
+    while (Serial.available() == 0); // Attendre que l'utilisateur saisisse quelque chose
 
-      if (numero == (answer[i] - '0')) { // Convertit le caractère à un entier et compare
-        i++;
-        Serial.println("Correct !");
-      } else {
-        lives--;
-        Serial.println("Incorrect !");
-        if (lives == 0) {
-          Game = false;
-        }
+    // Lecture de l'entrée utilisateur
+    int numero = Serial.parseInt(); // Récupère un nombre entier depuis le port série
+
+    if (numero == (answer[i] - '0')) { // Convertit le caractère à un entier et compare
+      i++;
+      Serial.println("Correct !");
+    } else {
+      lives--;
+      Serial.println("Incorrect !");
+      if (lives == 0) {
+        Game = false;
       }
     }
   }
@@ -45,5 +46,8 @@ void loop() {
   } else if (i == 10) {
     Serial.println("Bien joué ! Vous avez gagné.");
     while (true); // Stoppe le programme
+  }
+}
+
   }
 }
