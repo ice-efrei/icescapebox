896da21776373fb32e6d5868325d63ad17b97f81
Grégoire <gbadiche@gmail.com>
2024-12-22 14:29:36 +0100

    prettied and fixed linkage error

--- lib/gameseed/GameSeed.cpp
+++ lib/gameseed/GameSeed.cpp
@@ -7,32 +7,43 @@
 #define MED_READ 15
 #define MED_READ_2 (MED_READ / 2)
 
-GameSeed::GameSeed(int pin) {
+GameSeed::GameSeed(int pin)
+{
     _pin = pin;
 }
 
-void swap(int *a, int *b) {
-    *a = *a ^ *b;
-    *b = *a ^ *b;
-    *a = *a ^ *b;
-}
+namespace
+{
+    void swap(int *a, int *b)
+    {
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+    }
 
-void bubbleSort(int *array, int size) {
-    for(int i = size - 1; i >= 0; i--) {
-        for(int j = 0; j < i; j++) {
-            if(array[j] > array[j + 1]) {
-                swap(&(array[j]), &(array[j + 1]));
+    void bubbleSort(int *array, int size)
+    {
+        for (int i = size - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    swap(&(array[j]), &(array[j + 1]));
+                }
             }
         }
     }
 }
 
-int GameSeed::getSeed() {
+int GameSeed::getSeed()
+{
     pinMode(_pin, INPUT);
 
     int readings[MED_READ];
 
-    for(int i = 0; i < MED_READ; i++) {
+    for (int i = 0; i < MED_READ; i++)
+    {
         readings[i] = analogRead(_pin);
         readings[i] = readings[i] >> 1;
         delay(10);
@@ -46,19 +57,22 @@ int GameSeed::getSeed() {
     return seed;
 }
 
-void GameSeed::begin() {
+void GameSeed::begin()
+{
     read();
 
     randomSeed(seed);
 
     // Generate a random 4-digit serial number in base 16
     serialNumber = String(random(0x10000), HEX);
-    while (serialNumber.length() < 4) {
+    while (serialNumber.length() < 4)
+    {
         serialNumber = "0" + serialNumber;
     }
 }
 
-int GameSeed::read() {
+int GameSeed::read()
+{
     seed = getSeed();
     return seed;
 }
--- src/wiregame/WireGame.cpp
+++ src/wiregame/WireGame.cpp
@@ -2,60 +2,67 @@
 #include "GameSeed.h"
 #include "wiregame/WireGame.h"
 
-int removeElement(int *array, int index, int *size) {
-    int res = array[index];
-    // lowkey need a goth mommy rn
-    for(int i = index; i < *size - 1; i++) array[i] = array[i + 1];
-    *size = *size - 1;
-    return res;
-}
-
-// Flemme de malloc et tout alors on utilise un array du stack
-void range(int *array, int start, int n) {
-    if(n > WIRE_COUNT) return;
-    for(int i = 0; i < n; i++) {
-        array[i] = start + i;
+namespace
+{
+    int removeElement(int *array, int index, int *size)
+    {
+        int res = array[index];
+        // lowkey need a goth mommy rn
+        for (int i = index; i < *size - 1; i++)
+            array[i] = array[i + 1];
+        *size = *size - 1;
+        return res;
     }
-}
 
-void swap(int *a, int *b) {
-    // im just a chill guy who don't like tmp variables
-    *a = *a ^ *b;
-    *b = *a ^ *b;
-    *a = *a ^ *b;
-}
+    void swap(int *a, int *b)
+    {
+        // im just a chill guy who don't like tmp variables
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+    }
 
-// BUBBLE SORT BABYYYY
-void sortWires(wire_t *wires, int size) {
-    for(int i = size - 1; i >= 0; i--)  {
-        for(int j = 0; j < i; j++) {
-            if(wires[j].pin > wires[j + 1].pin) {
-                swap(&(wires[j].pin), &(wires[j + 1].pin));
-            }   
+    // BUBBLE SORT BABYYYY
+    void sortWires(wire_t *wires, int size)
+    {
+        for (int i = size - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (wires[j].pin > wires[j + 1].pin)
+                {
+                    swap(&(wires[j].pin), &(wires[j + 1].pin));
+                }
+            }
         }
     }
 }
 
-WireGame::WireGame(int pins[WIRE_COUNT], RGBLed& led, GameSeed& seed): _led(led), _seed(seed) {
-    for(int i = 0; i < WIRE_COUNT; i++) {
+WireGame::WireGame(int pins[WIRE_COUNT], RGBLed &led, GameSeed &seed) : _led(led), _seed(seed)
+{
+    for (int i = 0; i < WIRE_COUNT; i++)
+    {
         _pins[i] = pins[i];
     }
 }
 
-void WireGame::generateWires() {
+void WireGame::generateWires()
+{
     // Generate a random number of wires between 3 and 6
     wireCount = 3 + random(4);
 
     int availablePins[WIRE_COUNT];
 
-    for(int i = 0; i < WIRE_COUNT; i++) {
+    for (int i = 0; i < WIRE_COUNT; i++)
+    {
         availablePins[i] = _pins[i];
     }
 
     int size = WIRE_COUNT;
 
     // Generate a random list of colors
-    for (int i = 0; i < wireCount; i++) {
+    for (int i = 0; i < wireCount; i++)
+    {
         wire_t wire;
         wire.color = (Color)random(5);
         wire.index = i;
@@ -66,9 +73,11 @@ void WireGame::generateWires() {
     sortWires(wireList, wireCount);
 }
 
-void WireGame::begin() {
+void WireGame::begin()
+{
 
-    for (int i = 0; i < WIRE_COUNT; i++) {
+    for (int i = 0; i < WIRE_COUNT; i++)
+    {
         pinMode(_pins[i], INPUT_PULLUP);
     }
 
@@ -77,19 +86,28 @@ void WireGame::begin() {
     wireToCut = determineWireToCut();
 }
 
-String WireGame::colorToStr(Color c) {
-    switch(c) {
-        case white : return "white";
-        case red: return "red";
-        case yellow : return "yellow";
-        case blue : return "blue";
-        case black : return "black";
-        default : return "black";
+String WireGame::colorToStr(Color c)
+{
+    switch (c)
+    {
+    case white:
+        return "white";
+    case red:
+        return "red";
+    case yellow:
+        return "yellow";
+    case blue:
+        return "blue";
+    case black:
+        return "black";
+    default:
+        return "black";
     }
 }
 
 // Écrit par Eliott (et reprise un peu par moi) si vous avez des revendications c'est pour lui
-wire_t WireGame::determineWireToCut() {
+wire_t WireGame::determineWireToCut()
+{
     bool hasRed = false;
     bool hasBlack = false;
     bool hasYellow = false;
@@ -99,91 +117,140 @@ wire_t WireGame::determineWireToCut() {
     int yellowCount = 0;
     int whiteCount = 0;
 
-    for (int i = 0; i < wireCount; i++) {
-        if (wireList[i].color == red) {
+    for (int i = 0; i < wireCount; i++)
+    {
+        if (wireList[i].color == red)
+        {
             hasRed = true;
             lastRedIndex = i;
         }
-        if (wireList[i].color == blue) {
+        if (wireList[i].color == blue)
+        {
             blueCount++;
             lastBlueIndex = i;
         }
-        if (wireList[i].color == yellow) {
+        if (wireList[i].color == yellow)
+        {
             yellowCount++;
         }
-            if (wireList[i].color == white) {
+        if (wireList[i].color == white)
+        {
             whiteCount++;
         }
-        if (wireList[i].color == black) {
+        if (wireList[i].color == black)
+        {
             hasBlack = true;
         }
     }
 
     int lastDigit = strtol(_seed.serialNumber.substring(_seed.serialNumber.length() - 1).c_str(), NULL, 16);
 
-    if (wireCount == 3) {
-        if (!hasRed) {
-            return wireList[1];  // Cut the second wire
-        } else if (wireList[2].color == white) {
-            return wireList[2];  // Cut the last wire
-        } else if (blueCount > 1) {
-            return wireList[lastBlueIndex];  // Cut the last blue wire
-        } else {
-            return wireList[2];  // Cut the last wire
+    if (wireCount == 3)
+    {
+        if (!hasRed)
+        {
+            return wireList[1]; // Cut the second wire
         }
-    } else if (wireCount == 4) {
-        if (hasRed && (lastDigit % 2 != 0)) {
-            return wireList[lastRedIndex];  // Cut the last red wire
-        } else if (wireList[3].color == yellow && !hasRed) {
-            return wireList[0];  // Cut the first wire
-        } else if (blueCount == 1) {
-            return wireList[0];  // Cut the first wire
-        } else if (yellowCount > 1) {
-            return wireList[3];  // Cut the last wire
-        } else {
-            return wireList[1];  // Cut the second wire
+        else if (wireList[2].color == white)
+        {
+            return wireList[2]; // Cut the last wire
         }
-    } else if (wireCount == 5) {
-        if (wireList[4].color == black && (lastDigit % 2 != 0)) {
-            return wireList[3];  // Cut the fourth wire
-        } else if (hasRed && yellowCount > 1) {
-            return wireList[0];  // Cut the first wire
-        } else if (!hasBlack) {
-            return wireList[1];  // Cut the second wire
-        } else {
-            return wireList[0];  // Cut the first wire
+        else if (blueCount > 1)
+        {
+            return wireList[lastBlueIndex]; // Cut the last blue wire
         }
-    } else if (wireCount == 6) {
-        if (!hasYellow && (lastDigit % 2 != 0)) {
-            return wireList[2];  // Cut the third wire
-        } else if (yellowCount == 1 && whiteCount > 1) {
-            return wireList[3];  // Cut the fourth wire
-        } else if (!hasRed) {
-            return wireList[5];  // Cut the last wire
-        } else {
-            return wireList[3];  // Cut the fourth wire
+        else
+        {
+            return wireList[2]; // Cut the last wire
+        }
+    }
+    else if (wireCount == 4)
+    {
+        if (hasRed && (lastDigit % 2 != 0))
+        {
+            return wireList[lastRedIndex]; // Cut the last red wire
+        }
+        else if (wireList[3].color == yellow && !hasRed)
+        {
+            return wireList[0]; // Cut the first wire
+        }
+        else if (blueCount == 1)
+        {
+            return wireList[0]; // Cut the first wire
+        }
+        else if (yellowCount > 1)
+        {
+            return wireList[3]; // Cut the last wire
+        }
+        else
+        {
+            return wireList[1]; // Cut the second wire
+        }
+    }
+    else if (wireCount == 5)
+    {
+        if (wireList[4].color == black && (lastDigit % 2 != 0))
+        {
+            return wireList[3]; // Cut the fourth wire
+        }
+        else if (hasRed && yellowCount > 1)
+        {
+            return wireList[0]; // Cut the first wire
+        }
+        else if (!hasBlack)
+        {
+            return wireList[1]; // Cut the second wire
+        }
+        else
+        {
+            return wireList[0]; // Cut the first wire
+        }
+    }
+    else if (wireCount == 6)
+    {
+        if (!hasYellow && (lastDigit % 2 != 0))
+        {
+            return wireList[2]; // Cut the third wire
+        }
+        else if (yellowCount == 1 && whiteCount > 1)
+        {
+            return wireList[3]; // Cut the fourth wire
+        }
+        else if (!hasRed)
+        {
+            return wireList[5]; // Cut the last wire
+        }
+        else
+        {
+            return wireList[3]; // Cut the fourth wire
         }
     }
 
-  return wireList[0];  // Should never reach here
+    return wireList[0]; // Should never reach here
 }
 
-int WireGame::checkWin() {
+int WireGame::checkWin()
+{
     int c = 0;
-    for (int i = 0; i < wireCount; i++) {
+    for (int i = 0; i < wireCount; i++)
+    {
         int state = digitalRead(_pins[i]);
-        if (state == HIGH && _pins[i] != wireToCut.pin) {
+        if (state == HIGH && _pins[i] != wireToCut.pin)
+        {
             return -1;
         }
-        if(state == HIGH && _pins[i] == wireToCut.pin) {
+        if (state == HIGH && _pins[i] == wireToCut.pin)
+        {
             c += 1;
         }
     }
     return c;
 }
 
-void WireGame::connectWires() {
-    for(int i = 0; i < wireCount; i++) {
+void WireGame::connectWires()
+{
+    for (int i = 0; i < wireCount; i++)
+    {
         Serial.print("Wire ");
         Serial.print(colorToStr(wireList[i].color));
         Serial.print(" at pin ");
@@ -194,7 +261,8 @@ void WireGame::connectWires() {
         _led.color(wireList[i].color);
 
         int c = true;
-        while(c) {
+        while (c)
+        {
             c = digitalRead(wireList[i].pin);
             delay(100);
         }
@@ -205,7 +273,8 @@ void WireGame::connectWires() {
 
 // --------
 
-RGBLed::RGBLed(int R_G_B_pins[3]) {
+RGBLed::RGBLed(int R_G_B_pins[3])
+{
     redPin = R_G_B_pins[0];
     greenPin = R_G_B_pins[1];
     bluePin = R_G_B_pins[2];
@@ -214,47 +283,52 @@ RGBLed::RGBLed(int R_G_B_pins[3]) {
     _blue = 0;
 }
 
-void RGBLed::begin() {
+void RGBLed::begin()
+{
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
     reset();
 }
 
-void RGBLed::update() {
+void RGBLed::update()
+{
     digitalWrite(redPin, !_red);
     digitalWrite(greenPin, !_green);
     digitalWrite(bluePin, !_blue);
 }
 
-void RGBLed::reset() {
+void RGBLed::reset()
+{
     red(LOW);
     blue(LOW);
     green(LOW);
 }
 
 // WireGame specific function
-void RGBLed::color(Color c) {
+void RGBLed::color(Color c)
+{
     reset();
-    switch(c) { 
-        case 0: // White
-            red(HIGH);
-            blue(HIGH);
-            green(HIGH);
-            break;
-        case 1: // Red
-            red(HIGH);
-            break;
-        case 2: // Yellow
-            red(HIGH);
-            green(HIGH);
-            break;
-        case 3: // Blue
-            blue(HIGH);
-            break;
-        case 4: // Black
-            // Green is black
-            green(HIGH);
-            break;
+    switch (c)
+    {
+    case 0: // White
+        red(HIGH);
+        blue(HIGH);
+        green(HIGH);
+        break;
+    case 1: // Red
+        red(HIGH);
+        break;
+    case 2: // Yellow
+        red(HIGH);
+        green(HIGH);
+        break;
+    case 3: // Blue
+        blue(HIGH);
+        break;
+    case 4: // Black
+        // Green is black
+        green(HIGH);
+        break;
     }
 }
