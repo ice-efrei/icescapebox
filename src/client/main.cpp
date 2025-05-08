b904b288ee69444f4a01fd1982a9a3e3f92f526d
Grégoire <puma-cattle-bunch@duck.com>
2025-05-08 18:49:57 +0200

    updated master code using all the new libraries

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,32 +1,24 @@
 #include <Arduino.h>
-#include "Telegraph.h"
+#include "ICEscapeBox.h"
 
-#define TX 2
-#define RX 3
+// #define TX 2
+// #define RX 3
 
-using namespace telegraph;
-
-Telegraph wire = Telegraph();
-
-byte res;
-byte data[3] = {'1', '2', '3'};
-byte b = 128;
 
 void setup()
 {
     Serial.begin(9600);
-    wire.listen(3, 1);
-    wire.talk(2, 100);
-    wire.begin();
-    // wire.await_all();
-    delay(2000);
-    wire.tx().send(data, 3);
+    Serial.println("Waiting...");
+    ieb.begin();
+    Serial.print("Random seed is ");
+    Serial.print(ieb.seed);
+    Serial.print(", serial number is ");
+    Serial.print(ieb.serialNumber);
+    Serial.print(", ms is ");
+    Serial.println(ieb.ms());
 }
 
 void loop()
 {
-    if (wire.tx().buff_size() == 0) {
-        wire.tx().send(data, 3);
-    }
-    wire.tick();
+    
 }
--- src/master/main.cpp
+++ src/master/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include "Telegraph.h"
+#include "TM1650.h"
+#include "IEBRandom.h"
 
 #define TX 2
 #define RX 3
@@ -8,32 +10,90 @@ using namespace telegraph;
 
 Telegraph wire = Telegraph();
 
-byte res;
-byte data[3] = {'h', 'i', '!'};
+TM1650 clock(9, 10, 4);
+
+byte lifes = 3;
+
+const int totalTime = 6000;
+
+void updateClock() {
+    int nTime = totalTime - (ms() / 10) % 10000;
+    clock.setDisplayToDecNumber(nTime, 0x4);
+    const int mil = nTime % 100;
+    if (mil < 2) {
+        digitalWrite(12, HIGH);
+    } else {
+        digitalWrite(12, LOW);
+    }
+}
+
+void waitForStart() {
+
+}
+
+void explode() {
+    Serial.println("Exploding!");
+}
+
+void disarm() {
+    Serial.println("Disarming!");
+}
 
 void setup()
 {
     Serial.begin(9600);
     wire.listen(3, 100);
     wire.listen(4, 100);
+
     wire.talk(2, 100);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    pinMode(12, OUTPUT);
+    digitalWrite(12, LOW);
     wire.begin();
     Serial.println("waiting");
-    wire.await_all();
+
+    waitForStart();
+
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("ready");
+    seed = analogRead(0);
+
+    wire.tx().tell((byte *)&seed, sizeof(seed));
+    
+    genRandomsValues(seed);
+
+    Serial.print("Random seed is ");
+    Serial.print(seed);
+    Serial.print(", serial number is ");
+    Serial.print(serialNumber);
+    Serial.print(", ms is ");
+    Serial.println(ms());
+
+    clock.setupDisplay(true, 7);
+
 }
 
 void loop()
 {
-    for (unsigned short i = 0; i < wire.n_listeners; i++) {
+    wire.tick();
+    for (byte i = 0; i < 2; i++) {
         if (wire.rxs(i).buff_size() != 0) {
-            Serial.print("buff size : ");
-            Serial.print(wire.rxs(i).buff_size());
-            Serial.print(", wire ");
-            Serial.print(i);
-            Serial.print(" said ");
-            Serial.println((char) wire.rxs(i).read());
+            byte val = wire.rxs(i).read();
+            if (val == 0x0) {
+                explode();
+            }
+            if (val == 0xf) {
+                disarm();
+            }
+            if ((val & 0xc) == 0x80) {
+                byte n = val & 0x3;
+                lifes -= n;
+                if (lifes < 0) {
+                    lifes = 0;
+                }
+            }
         }
     }
-    wire.tick();
+    updateClock();
 }
